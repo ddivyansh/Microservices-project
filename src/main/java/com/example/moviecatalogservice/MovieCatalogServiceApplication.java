d9a52dd313f14b932153020ec8d71e0326650a26
Divyansh Dheer <divyanshdheer42@gmail.com>
2022-07-30 22:25:26 +0530

    Day one changes in mv catalog service

--- src/main/java/com/example/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/example/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,10 +2,24 @@ package com.example.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
+	//Rest template builder :
+	@Bean
+	RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 
+	@Bean
+	WebClient.Builder getWebClient()
+	{
+		WebClient.Builder builder = WebClient.builder();
+		return builder;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
--- src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,19 +1,56 @@
 package com.example.moviecatalogservice.resources;
 
 import com.example.moviecatalogservice.models.CatalogItem;
+import com.example.moviecatalogservice.models.Movie;
+import com.example.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+    @Autowired
+    private RestTemplate restTemplate; //allows us to connect to a URL.
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;//Same as rest template but reactive.
+
     @RequestMapping("/{userId}")
-    public List<CatalogItem> getCatlog(@PathVariable String userId){
-        return Collections.singletonList(new CatalogItem("Jujutsu Kaisen", "Anime movie about curses", 5));
+    public List<CatalogItem> getCatlog(@PathVariable String userId) {
+        //URL to get info : http://localhost:8082/movies/a015
+
+        //Rated movies watched by userId
+        List<Rating> ratings = Arrays.asList(
+                new Rating(5, "A0001"),
+                new Rating(6, "A0002")
+        );
+
+        //Now we have to transform the above list into a list of CatalogItem
+        return ratings.stream().map(
+                        rating -> {
+//                            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                            Movie movie = webClientBuilder.build()
+                                    .get()
+                                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                                    .retrieve()
+                                    .bodyToMono(Movie.class)
+                                    .block();
+
+                            return new CatalogItem(movie.getName(), "Desc", rating.getRatedValue());
+
+                        }
+                )
+                .collect(Collectors.toList());
+
     }
 }
